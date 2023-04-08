4133b260b22f9b7d0c0137ab334e18172c204a19
AnnaK <anna_morozova_kz@list.ru>
2023-04-08 13:42:41 +0300

    Exercise_2

--- src/test/java/ru/netology/hw_cycles/services/MonthCountServiceTest.java
+++ src/test/java/ru/netology/hw_cycles/services/MonthCountServiceTest.java
@@ -2,21 +2,13 @@ package ru.netology.hw_cycles.services;
 
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
-import org.junit.jupiter.params.provider.CsvSource;
-import org.testng.annotations.Test;
 import org.junit.jupiter.api.Assertions;
+
 public class MonthCountServiceTest {
 
     @ParameterizedTest
 
-//    @CsvSource({
-//            "3, 10000, 3000, 20000",
-//            "2, 100000, 60000, 150000"
-//
-//    })
-
-    @CsvFileSource(files = "src/test/resources/Values")
-
+    @CsvFileSource(files = "src/test/resources/values.csv")
     public void testFirst(int expected, int income, int expenses, int threshold) {
 
         MonthCountService service = new MonthCountService();
@@ -38,13 +30,15 @@ public class MonthCountServiceTest {
 //        MonthCountService service = new MonthCountService();
 //
 //        int expected = 2;
-//        int income = 100_000; //доход
-//        int expenses = 60_000; //месячные траты
-//        int threshold = 150_000; //можно отдохнуть
+//        int income = 100000; //доход
+//        int expenses = 60000; //месячные траты
+//        int threshold = 150000; //можно отдохнуть
 //
 //        int actual = service.calculate(income, expenses, threshold);
 //
 //        Assertions.assertEquals(expected, actual);
+//
 //    }
+
 
 }
